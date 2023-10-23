58f9aadd8fa199cf7f8c1302563679df4db15860
Gruugydyz <136304036+Gruugydyz@users.noreply.github.com>
2023-10-23 17:53:53 +0800

    Add files via upload

---
+++ FrmBasicThread1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmBasicThread1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private Thread ThreadA, ThreadB, ThreadC, ThreadD;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart start1 = new ThreadStart(MyThreadClass.Thread1);
+            ThreadStart start2 = new ThreadStart(MyThreadClass.Thread2);    
+            ThreadA = new Thread(start1);
+            ThreadB = new Thread(start2);
+            ThreadC = new Thread(start1);
+            ThreadD = new Thread(start2);
+
+            ThreadA.Name = "Thread A Process ";
+            ThreadB.Name = "Thread B Process ";
+            ThreadC.Name = "Thread C Process ";
+            ThreadD.Name = "Thread D Process ";
+            ThreadA.Priority = ThreadPriority.Highest;
+            ThreadB.Priority = ThreadPriority.Normal;
+            ThreadC.Priority = ThreadPriority.AboveNormal;
+            ThreadD.Priority = ThreadPriority.BelowNormal;
+            Console.WriteLine(label1.Text.ToString());
+
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadC.Start();
+            ThreadD.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+            ThreadC.Join();
+            ThreadD.Join();
+            label1.Text = "-End of Thread-";
+            Console.WriteLine(label1.Text.ToString());
+
+
+        }
+    }
+}
---
+++ FrmBasicThread1/MyThreadClass.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrmBasicThread1
+{
+
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+
+            for (int loopCount = 0; loopCount <= 2; loopCount ++)
+            {
+
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+                Thread.Sleep(1000);
+            }
+
+        }
+        public static void Thread2()
+        {
+            for (int loopCount = 0; loopCount <= 5; loopCount++)
+            {
+
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+                Thread.Sleep(2000);
+            }
+
+
+        }
+
+
+    }
+}
